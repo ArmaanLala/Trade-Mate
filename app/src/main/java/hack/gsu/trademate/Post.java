51313f8fb2ece511d056e99eecde90226f293450
Armaan Lala <armaanlala@gmail.com>
2019-11-02 10:52:23 -0400

    Added App Icon and finished the Post class

--- app/src/main/java/hack/gsu/trademate/Post.java
+++ app/src/main/java/hack/gsu/trademate/Post.java
@@ -1,4 +1,60 @@
 package hack.gsu.trademate;
 
+import java.util.Date;
+
 public class Post {
+    public String name,imageUrl,cost,imageThumb;
+    public Date currentTime;
+
+    public Post () {
+
+    }
+
+    public Post ( String name,String imageUrl,String cost,String imageThumb, Date date){
+        this.name = name;
+        this.imageUrl=imageUrl;
+        this.cost= cost;
+        this.imageThumb = imageThumb;
+        this.currentTime=date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageThumb() {
+        return imageThumb;
+    }
+
+    public void setImageThumb(String imageThumb) {
+        this.imageThumb = imageThumb;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Date getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(Date currentTime) {
+        this.currentTime = currentTime;
+    }
 }
